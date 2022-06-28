cbafa44a61d4ded06a0c2f1854d04c23ba67ba42
Yuval Shmilovich <yuvalshm@on.huji.ac.il>
2022-06-29 01:11:40 +0300

    adding solvefortime functions

---
+++ solvefortime_main.m
@@ -0,0 +1,13 @@
+
+
+%% Load gully data
+load('gully_data.mat')
+
+%% Defiend parameters for the simulation
+input_parameters.m = 0.5;            % Area exponent [-]
+input_parameters.n = 1.5;            % Slope exponent [-]
+input_parameters.K = 5*10^-5;        % Erodibility coeff. [m^(1-2m) yr^-1]
+input_parameters.D =0.0;             % Diffusion coeff [m^2/yr] 
+input_parameters.dt = 1000;          % Model time step [yr], this may be modified internaly
+dts = [10000:5000:100000];           % Model elapsed times to run [year]
+[simulation_struc] = solvefortime(input_parameters,dts,data);
