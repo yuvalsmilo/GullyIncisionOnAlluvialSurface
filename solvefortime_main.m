f16c0203b8e6d1655c6f323359ce0065d825b22b
Yuval Shmilovich <yuvalshm@on.huji.ac.il>
2022-12-01 21:23:56 +0200

    fix diffusivity

--- solvefortime_main.m
+++ solvefortime_main.m
@@ -7,7 +7,7 @@ load('gully_data.mat')
 input_parameters.m = 0.5;            % Area exponent [-]
 input_parameters.n = 1.5;            % Slope exponent [-]
 input_parameters.K = 5*10^-5;        % Erodibility coeff. [m^(1-2m) yr^-1]
-input_parameters.D =0.0;             % Diffusion coeff [m^2/yr] 
+input_parameters.D =10^-10;             % Diffusion coeff [m^2/yr] 
 input_parameters.dt = 1000;          % Model time step [yr], this may be modified internaly
 dts = [10000:5000:100000];           % Model elapsed times to run [year]
 [simulation_struc] = solvefortime(input_parameters,dts,data);
